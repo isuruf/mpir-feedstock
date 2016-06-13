abad7329cad02f034d7b1b8f12ccdca02aea9148
Isuru Fernando <isuruf@gmail.com>
2016-06-13 20:36:29 +0530

    Add yasm to path

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,11 +4,13 @@ source activate "${CONDA_DEFAULT_ENV}"
 
 chmod +x configure
 
+export PATH=$PATH:$PREFIX/bin
+
 if [ "$(uname)" == "Darwin" ];
 then
-    ./configure --prefix=$PREFIX --enable-cxx --build=x86_64-apple-darwin --with-yasm=$PREFIX/bin/yasm
+    ./configure --prefix=$PREFIX --enable-cxx --build=x86_64-apple-darwin --with-system-yasm=yes
 else
-    ./configure --prefix=$PREFIX --enable-cxx --enable-fat --with-yasm=$PREFIX/bin/yasm
+    ./configure --prefix=$PREFIX --enable-cxx --enable-fat --with-system-yasm=yes
 
 fi
 
