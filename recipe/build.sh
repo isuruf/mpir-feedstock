ca7cd6ce389a26d6f2e5ea01dedd03fc528acada
Isuru Fernando <isuruf@gmail.com>
2016-06-13 20:16:34 +0530

    Full path for yasm executable

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,9 +6,9 @@ chmod +x configure
 
 if [ "$(uname)" == "Darwin" ];
 then
-    ./configure --prefix=$PREFIX --enable-cxx --build=x86_64-apple-darwin --with-yasm=$PREFIX
+    ./configure --prefix=$PREFIX --enable-cxx --build=x86_64-apple-darwin --with-yasm=$PREFIX/bin/yasm
 else
-    ./configure --prefix=$PREFIX --enable-cxx --enable-fat --with-yasm=$PREFIX
+    ./configure --prefix=$PREFIX --enable-cxx --enable-fat --with-yasm=$PREFIX/bin/yasm
 
 fi
 
