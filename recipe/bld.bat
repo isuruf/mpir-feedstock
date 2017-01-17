ad0c30465e1f1b1b2c952e673d75e6a86266f90d
Isuru Fernando <isuruf@gmail.com>
2017-01-17 22:49:51 +0530

    Use VS_MAJOR

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,17 +1,4 @@
-REM write a temporary batch file to map cl.exe version to visual studio version
-echo @echo 16=10>> msvc_versions.bat
-echo @echo 19=14>> msvc_versions.bat
-
-REM Run cl.exe to find which version our compiler is
-for /f "delims=" %%A in ('cl /? 2^>^&1 ^| findstr /C:"Version"') do set "CL_TEXT=%%A"
-FOR /F "tokens=1,2 delims==" %%i IN ('msvc_versions.bat') DO echo %CL_TEXT% | findstr /C:"Version %%i" > nul && set VSTRING=%%j && goto FOUND
-EXIT 1
-:FOUND
-
-REM Trim trailing whitespace that may prevent CMake from finding which generator to use
-call :TRIM VSTRING %VSTRING%
-
-cd build.vc%VSTRING%
+cd build.vc%VS_MAJOR%
 
 if "%ARCH%"=="32" (
     set PLATFORM=Win32
@@ -59,10 +46,3 @@ move lib\%PLATFORM%\%BUILD_TYPE%\longlong.h %LIBRARY_INC%\gmp-longlong.h
 
 dir %LIBRARY_INC%
 dir %LIBRARY_LIB%
-
-:TRIM
-  SetLocal EnableDelayedExpansion
-  set Params=%*
-  for /f "tokens=1*" %%a in ("!Params!") do EndLocal & set %1=%%b
-  exit /B
-
