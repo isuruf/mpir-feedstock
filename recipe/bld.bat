d9f8cdf2fac327b651066425e947126b6357b85b
Isuru Fernando <isuruf@gmail.com>
2017-06-24 23:46:27 +0530

    MPIR debug

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -20,24 +20,24 @@ if "%ARCH%"=="32" (
 )
 
 REM build static library
-msbuild.exe /p:Platform=%PLATFORM% /p:Configuration=Release lib_mpir_gc\lib_mpir_gc.vcxproj
-msbuild.exe /p:Platform=%PLATFORM% /p:Configuration=Release lib_mpir_cxx\lib_mpir_cxx.vcxproj
+msbuild.exe /p:Platform=%PLATFORM% /p:Configuration=Debug lib_mpir_gc\lib_mpir_gc.vcxproj
+msbuild.exe /p:Platform=%PLATFORM% /p:Configuration=Debug lib_mpir_cxx\lib_mpir_cxx.vcxproj
 REM build dll library, cxx is also included.
-msbuild.exe /p:Platform=%PLATFORM% /p:Configuration=Release dll_mpir_gc\dll_mpir_gc.vcxproj
+msbuild.exe /p:Platform=%PLATFORM% /p:Configuration=Debug dll_mpir_gc\dll_mpir_gc.vcxproj
 
 if not exist "%LIBRARY_LIB%" mkdir %LIBRARY_LIB%
 if not exist "%LIBRARY_INC%" mkdir %LIBRARY_INC%
 if not exist "%LIBRARY_BIN%" mkdir %LIBRARY_BIN%
 
 REM move .lib, .dll and .pdb files to LIBRARY_LIB
-move lib_mpir_gc\%PLATFORM%\Release\mpir.lib %LIBRARY_LIB%\mpir_static.lib
-move lib_mpir_gc\%PLATFORM%\Release\mpir.pdb %LIBRARY_BIN%\mpir_static.pdb
-move lib_mpir_cxx\%PLATFORM%\Release\mpirxx.lib %LIBRARY_LIB%\mpirxx_static.lib
-move lib_mpir_cxx\%PLATFORM%\Release\mpirxx.pdb %LIBRARY_BIN%\mpirxx_static.pdb
+move lib_mpir_gc\%PLATFORM%\Debug\mpir.lib %LIBRARY_LIB%\mpir_static.lib
+move lib_mpir_gc\%PLATFORM%\Debug\mpir.pdb %LIBRARY_BIN%\mpir_static.pdb
+move lib_mpir_cxx\%PLATFORM%\Debug\mpirxx.lib %LIBRARY_LIB%\mpirxx_static.lib
+move lib_mpir_cxx\%PLATFORM%\Debug\mpirxx.pdb %LIBRARY_BIN%\mpirxx_static.pdb
 
-move dll_mpir_gc\%PLATFORM%\Release\mpir.lib %LIBRARY_LIB%\mpir.lib
-move dll_mpir_gc\%PLATFORM%\Release\mpir.dll %LIBRARY_BIN%\mpir.dll
-move dll_mpir_gc\%PLATFORM%\Release\mpir.pdb %LIBRARY_BIN%\mpir.pdb
+move dll_mpir_gc\%PLATFORM%\Debug\mpir.lib %LIBRARY_LIB%\mpir.lib
+move dll_mpir_gc\%PLATFORM%\Debug\mpir.dll %LIBRARY_BIN%\mpir.dll
+move dll_mpir_gc\%PLATFORM%\Debug\mpir.pdb %LIBRARY_BIN%\mpir.pdb
 
 REM create gmp libs to be compatible
 copy %LIBRARY_LIB%\mpir_static.lib %LIBRARY_LIB%\gmp_static.lib
@@ -47,15 +47,15 @@ copy %LIBRARY_BIN%\mpir.dll %LIBRARY_BIN%\gmp.dll
 
 cd ..
 REM move .h files to LIBRARY_INC
-move lib\%PLATFORM%\Release\mpir.h %LIBRARY_INC%
-move lib\%PLATFORM%\Release\mpirxx.h %LIBRARY_INC%
-move lib\%PLATFORM%\Release\gmp.h %LIBRARY_INC%
-move lib\%PLATFORM%\Release\gmpxx.h %LIBRARY_INC%
+move lib\%PLATFORM%\Debug\mpir.h %LIBRARY_INC%
+move lib\%PLATFORM%\Debug\mpirxx.h %LIBRARY_INC%
+move lib\%PLATFORM%\Debug\gmp.h %LIBRARY_INC%
+move lib\%PLATFORM%\Debug\gmpxx.h %LIBRARY_INC%
 
-move lib\%PLATFORM%\Release\config.h %LIBRARY_INC%\gmp-config.h
-move lib\%PLATFORM%\Release\gmp-impl.h %LIBRARY_INC%
-move lib\%PLATFORM%\Release\gmp-mparam.h %LIBRARY_INC%
-move lib\%PLATFORM%\Release\longlong.h %LIBRARY_INC%\gmp-longlong.h
+move lib\%PLATFORM%\Debug\config.h %LIBRARY_INC%\gmp-config.h
+move lib\%PLATFORM%\Debug\gmp-impl.h %LIBRARY_INC%
+move lib\%PLATFORM%\Debug\gmp-mparam.h %LIBRARY_INC%
+move lib\%PLATFORM%\Debug\longlong.h %LIBRARY_INC%\gmp-longlong.h
 
 dir %LIBRARY_INC%
 dir %LIBRARY_LIB%
