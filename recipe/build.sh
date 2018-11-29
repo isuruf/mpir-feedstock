3318f690476a4af2afcb380d4aa5b22f896a0df8
Anthony Scopatz <scopatz@gmail.com>
2018-11-29 18:05:46 -0500

    Add -fPIC flag

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,6 +6,8 @@ if [ "$(uname)" == "Darwin" ];
 then
     ./configure --prefix=$PREFIX --enable-pic --enable-cxx --build=x86_64-apple-darwin
 else
+    export CFLAGS="${CFLAGS} -fPIC"
+    export CXXFLAGS="${CXXFLAGS} -fPIC"
     ./configure --prefix=$PREFIX --enable-pic --enable-cxx --enable-fat
 fi
 
