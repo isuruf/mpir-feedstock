c6e6361d70a37c54e452067026861c4af63c1ebd
Isuru Fernando <isuruf@gmail.com>
2018-11-30 01:57:34 -0600

    Use CC in run test

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -1,2 +1,2 @@
-cc -I $PREFIX/include -L $PREFIX/lib test.c -lmpir -Wl,-rpath,$PREFIX/lib -o test.out
+${CC} -I $PREFIX/include -L $PREFIX/lib test.c -lmpir -Wl,-rpath,$PREFIX/lib -o test.out
 ./test.out
