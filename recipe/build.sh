2827db871f83270254065c17ca1a986c29c786a2
Isuru Fernando <isuruf@gmail.com>
2019-11-24 23:25:16 +0000

    Fix link and regenerate

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+
 chmod +x configure
 
 if [[ "$target_platform" == "osx-64" ]]; then
     ./configure --prefix=$PREFIX --with-pic --enable-cxx --build=x86_64-apple-darwin
 elif [[ "$target_platform" == "win-64" ]]; then
+    autoreconf -i
+    chmod +x configure
     ./configure --prefix=$PREFIX --with-pic --enable-cxx --enable-fat --disable-static --enable-gmpcompat
 else
     ./configure --prefix=$PREFIX --with-pic --enable-cxx --enable-fat
@@ -12,7 +15,7 @@ fi
 
 [[ "$target_platform" == "win-64" ]] && patch_libtool
 [[ "$target_platform" == "win-64" ]] && sed -i.bak "s@-link -DLL@shared@g" libtool
-[[ "$target_platform" == "win-64" ]] && ln -s $BUILD_PREFIX/bin/llvm-lib $BUILD_PREFIX/bin/lib
+[[ "$target_platform" == "win-64" ]] && ln -s $BUILD_PREFIX/bin/llvm-lib.exe $BUILD_PREFIX/Library/bin/lib.exe
 
 make -j${CPU_COUNT}
 make check -j${CPU_COUNT}
